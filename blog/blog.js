613deec156706fdf10485ea8dcc13cb8249a4647
James Hall-Treworgy <James Hall-Treworgy>
2016-02-18 20:57:37 -0500

    Changed blog colors and worked on implementing a forEach to display each blog post.

---
+++ blog/blog.js
@@ -0,0 +1,20 @@
+var blogContent = [
+  {
+  date: "November 23rd, 2015",
+  title: "Coding: Best Practices",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  }
+];
+
+blogContent.forEach(function(data) {
+  console.log(data.date);
+  console.log(data.title);
+  console.log(data.content);
+
+});
+
+
+
+  data.date
+  data.title
+  data.content
